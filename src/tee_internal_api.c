c04dc5ad52fea446ae69c4edc7fa3ff51dc71071
Rahul Mahadev <rahul.mahadev7@gmail.com>
2016-06-08 14:17:21 +0530

    sample block param passing

--- src/tee_internal_api.c
+++ src/tee_internal_api.c
@@ -4,25 +4,29 @@
  */
 #include "tee_internal_api.h"
  void init(){
- 	seL4_Word sender_badge;
-    seL4_MessageInfo_t tag;
-    seL4_Word msg,func_id;
-
-    tag = seL4_Recv(EP_CPTR,&sender_badge);
-    // assert(sender_badge == EP_CPTR );
-    // assert(seL4_MessageInfo_get_length(tag) == 1);
-    msg = seL4_GetMR(0);
-    func_id = seL4_GetMR(1);
-    printf("ta: got message from %#d from tee-container, function id = %d  \n",msg,func_id);
-    tee_result result = function_handler(msg,func_id);
-    if(result.status){
-    	seL4_SetMR(0, msg+1);
-    	seL4_ReplyRecv(EP_CPTR,tag,&sender_badge);	
-    }else{
-    	printf("Error! invalid reference to function\n");
-    }
+ 	while(1){
+ 		seL4_Word sender_badge;
+	    seL4_MessageInfo_t tag;
+	    seL4_Word msg,func_id;
+	    printf("receiver...........\n");
+	 	tag = seL4_Recv(EP_CPTR,&sender_badge);
+	    msg = seL4_GetMR(0);
+	    func_id = seL4_GetMR(1);
+	    int length = seL4_GetMR(2);
+	    for(int i = 0 ; i < length; ++i){
+	    	int temp = seL4_GetMR(i+3);
+	    	printf("  	%d \n",temp );
+	    }
+	    printf("ta: got message from %#d from tee-container, function id = %d  \n",msg,func_id);
+	    tee_result result = function_handler(msg,func_id);
+	    if(result.status){
+	    	seL4_SetMR(0, msg+1);
+	    	seL4_ReplyRecv(EP_CPTR,tag,&sender_badge);	
+	    }else{
+	    	printf("Error! invalid reference to function\n");
+	    }	
+ 	}
     
-
  }
 
  tee_result tee_make_result(int val){
