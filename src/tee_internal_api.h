57c43bc3fe99526086ef664b2004da634866b8a4
Rahul Mahadev <rahul.mahadev7@gmail.com>
2016-08-17 21:11:43 +0530

    Update tee_internal_api.h

--- src/tee_internal_api.h
+++ src/tee_internal_api.h
@@ -15,7 +15,7 @@ typedef struct {
 	void *data;
 	size_t size;
 }tee_result;
-#define EP_CPTR 0x3 // where the cap for the endpoint was placed.
+#define EP_CPTR 0x5 // where the cap for the endpoint was placed.
 extern tee_result function_handler(int msg,int func_id,seL4_Word* arr,int length);
 tee_result tee_make_result(int,void*,size_t);
 tee_result tee_ta_failure();
